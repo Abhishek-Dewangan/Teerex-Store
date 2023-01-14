ece86364041542d8a5ed87bf1e66987509bc1940
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-01-15 01:30:54 +0530

    Filter bar added

---
+++ src/Component/FilterBar/FilterBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './FilterBar.module.css'
+
+const FilterBar = () => {
+  return (
+    <div className={styles.main}>FilterBar</div>
+  )
+}
+
+export default FilterBar
